b21ef369de877071cfe5ea455301cf41a0ade1ed
pankajprasad30 <pankajprasad30@gmail.com>
2024-02-23 14:01:01 +0530

    program folder wise

---
+++ Java_Programs/Constructor.java
@@ -0,0 +1,19 @@
+package classroom_program;
+
+public class Constructor 
+{
+	Constructor()
+	{
+		System.out.println("Pankaj");//constructor
+	}
+
+	public static void main(String[] args) 
+	{
+		Constructor a1=new Constructor ();// 1st method to call constructor
+		new Constructor ();// 2nd method to call constructor
+		
+		
+
+	}
+
+}
